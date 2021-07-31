7ef79ee53eed6c125f2424fbf301e5202bbebc6f
Devon Poston <dev.poston@gmail.com>
2021-07-31 13:12:03 -0700

    refactoring stress test

--- k6script.js
+++ k6script.js
@@ -2,25 +2,6 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 // GET
-// export let options = {
-//   scenarios: {
-//     constant_request_rate: {
-//       executor: "constant-arrival-rate",
-//       rate: 1000,
-//       timeUnit: "1s",
-//       duration: "30s",
-//       preAllocatedVUs: 100,
-//       maxVUs: 200,
-//     },
-//   },
-// };
-// export default function () {
-//   let rndId = Math.floor(Math.random() * (10000000 - 9000000) + 9000000);
-//   const BASE_URL = `http://localhost:3000/dp/${rndId}`;
-//   http.get(`${BASE_URL}`);
-// }
-
-// POST
 export let options = {
   scenarios: {
     constant_request_rate: {
@@ -34,8 +15,27 @@ export let options = {
   },
 };
 export default function () {
-  const BASE_URL = 'http://localhost:3000/images';
-  let rndPic = Math.floor(Math.random() * 1001);
-  let imgUrl = `https://picsum.photos/id/${rndPic}/200/300`;
-  http.post(`${BASE_URL}`, JSON.stringify({'images': [imgUrl]}), { headers: { 'Content-Type': 'application/json' } });
+  let rndId = Math.floor(Math.random() * (10000000 - 9000000) + 9000000);
+  const BASE_URL = `http://localhost:3000/images/${rndId}`;
+  http.get(`${BASE_URL}`);
 }
+
+// POST
+// export let options = {
+//   scenarios: {
+//     constant_request_rate: {
+//       executor: "constant-arrival-rate",
+//       rate: 1000,
+//       timeUnit: "1s",
+//       duration: "30s",
+//       preAllocatedVUs: 100,
+//       maxVUs: 400,
+//     },
+//   },
+// };
+// export default function () {
+//   const BASE_URL = 'http://localhost:3000/images';
+//   let rndPic = Math.floor(Math.random() * 1001);
+//   let imgUrl = `https://picsum.photos/id/${rndPic}/200/300`;
+//   http.post(`${BASE_URL}`, JSON.stringify({'images': [imgUrl]}), { headers: { 'Content-Type': 'application/json' } });
+// }
--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -25,12 +29,13 @@ app.get('/:productId', (req, res) => {
 });
 
 app.post('/images', (req, res) => {
-  res.sendFile(path.join(__dirname, '/./public/index.html'));
+  console.log(req.body);
+  axios.post('http://localhost:3003/images', req.body)
+    .then((data) => {
+      console.log('PROXY DATA:', data);
+      res.sendFile(path.join(__dirname, '/./public/index.html'));
+    })
 });
-
-// app.post('/images', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/./public/index.html'));
-// });
 
 app.listen(port, () => {
   console.log(`Server now listening at http://localhost:${port}`);
