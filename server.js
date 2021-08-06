ef09c135b315a49dc8d50e673331c87053f45033
Devon Poston <dev.poston@gmail.com>
2021-08-05 18:38:18 -0700

    deploy proxy

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const Images = 'http://18.144.11.40:3003';
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -28,14 +28,10 @@ app.get('/:productId', (req, res) => {
   res.sendFile(path.join(__dirname, '/./public/index.html'))
 });
 
-app.post('/images', (req, res) => {
-  console.log(req.body);
-  axios.post('http://localhost:3003/images', req.body)
-    .then((data) => {
-      console.log('PROXY DATA:', data);
-      res.sendFile(path.join(__dirname, '/./public/index.html'));
-    })
-});
+app.use('/images', createProxyMiddleware({
+  target: Images,
+  changeOrigin: true
+}));
 
 app.listen(port, () => {
   console.log(`Server now listening at http://localhost:${port}`);
